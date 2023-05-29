9645e2de5e94205592366be7743f79e894a47b24
Sylvain Munaut <tnt@246tNt.com>
2023-05-29 19:55:42 +0000

    ol2/tt_block: Import User Block template OpenLane2 build
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

---
+++ ol2/tt_block/build.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+#
+# OpenLane2 build script to generate all TT blocks templates
+#
+# Copyright (c) 2023 Sylvain Munaut <tnt@246tNt.com>
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import glob
+import os
+import shutil
+import sys
+
+from typing import List, Type
+
+from openlane.flows.sequential import SequentialFlow
+from openlane.steps.odb import OdbpyStep
+from openlane.steps import (
+	Step,
+	Yosys,
+	OpenROAD,
+	Misc,
+)
+
+sys.path.append('../../py')
+import tt
+
+
+class IOPlacement(OdbpyStep):
+
+	id = "TT.Block.IOPlacement"
+	name = "Custom IO placement for TT User Blocks"
+
+	def get_script_path(self):
+		return os.path.join(
+			os.path.dirname(__file__),
+			"odb_ioplace.py"
+		)
+
+	def get_command(sef) -> List[str]:
+		return super().get_command()
+
+
+class BlockTemplateFlow(SequentialFlow):
+
+	Steps: List[Type[Step]] = [
+		Yosys.Synthesis,
+		Misc.LoadBaseSDC,
+		OpenROAD.Floorplan,
+		IOPlacement,
+	]
+
+
+def gen_block_template(tti, h_mult, v_mult):
+	# Get PDK root out of environment
+	PDK_ROOT = os.getenv('PDK_ROOT')
+
+	# Create directory
+	design_dir = f"{h_mult:d}x{v_mult:d}"
+	if not os.path.exists(design_dir):
+		os.mkdir(design_dir)
+
+	# Final block width and height
+	block_width  = (
+		 h_mult      * tti.layout.glb.block.width.units +
+		(h_mult - 1) * tti.layout.glb.margin.x.units
+	)
+
+	block_height = (
+		 v_mult      * tti.layout.glb.block.height.units +
+		(v_mult - 1) * tti.layout.glb.margin.y.units
+	)
+
+	# Create and run custom flow
+	flow_cfg = {
+		# Main design properties
+		"DESIGN_NAME"    : "tt_um_template",
+		"DESIGN_IS_CORE" : False,
+
+		# Sources
+		"VERILOG_FILES"  : [
+			"../../rtl/tt_um_template.v",
+		],
+
+		# Floorplanning
+		"DIE_AREA"           :  f"0 0 {block_width/1000:.3f} {block_height/1000:.3f}",
+		"FP_SIZING"          : "absolute",
+		"BOTTOM_MARGIN_MULT" : 1,
+		"TOP_MARGIN_MULT"    : 1,
+		"LEFT_MARGIN_MULT"   : 6,
+		"RIGHT_MARGIN_MULT"  : 6,
+	}
+
+	flow = BlockTemplateFlow(
+		flow_cfg,
+		design_dir = design_dir,
+		pdk_root   = PDK_ROOT,
+		pdk        = "sky130A",
+	)
+
+	flow.start()
+
+	# Collect and rename the build product
+	m = list(sorted(glob.glob(os.path.join(design_dir, 'runs', '*', 'final', 'def', 'tt_um_template.def'))))[-1]
+
+	shutil.copyfile(m, f"def/tt_block_{h_mult:d}x{v_mult:d}.def")
+
+
+if __name__ == '__main__':
+	# Load TinyTapeout
+	tti = tt.TinyTapeout(modules=False)
+
+	# Create destination directory
+	if not os.path.exists("def"):
+		os.mkdir("def")
+
+	# Generate block templates for all supported sizes
+	for v_mult in [ 1, 2 ]:
+		for h_mult in [ 1, 2, 4, 8 ]:
+			gen_block_template(tti, h_mult, v_mult)
---
+++ ol2/tt_block/odb_ioplace.py
@@ -0,0 +1,42 @@
+#
+# OpenDB script for custom IO placement for the TT user blocks
+#
+# Copyright (c) 2023 Sylvain Munaut <tnt@246tNt.com>
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import os
+import sys
+
+sys.path.append('../../py')
+import tt
+import tt_odb
+
+import click
+sys.path.insert(0, os.path.join(os.environ.get("OPENLANE_ROOT"), "scripts", "odbpy"))
+from reader import click_odb
+
+
+@click.command()
+@click_odb
+def io_place(
+	reader,
+):
+
+	# Load TinyTapeout
+	tti = tt.TinyTapeout(modules=False)
+
+	# Terminal name mapping
+	bterm_map = {b.getName(): b for b in reader.block.getBTerms()}
+
+	# Find die & layers
+	die_area = reader.block.getDieArea()
+	layer_ns = reader.tech.findLayer(tti.cfg.tt.spine.vlayer)
+
+	# User block bottom
+	for pn, pp in tti.layout.ply_block.items():
+		tt_odb.place_pin(die_area, layer_ns, bterm_map.pop(pn), pp, 'N')
+
+
+if __name__ == "__main__":
+	io_place()
