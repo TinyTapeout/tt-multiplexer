359a1f2f92043027ae3a2bee1ddc9056a83bd80d
Sylvain Munaut <tnt@246tNt.com>
2024-04-04 21:36:21 +0200

    py/tt: Add width=6 as a suported width in the placer
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- py/tt/placer.py
+++ py/tt/placer.py
@@ -166,7 +166,7 @@ class ModulePlacer:
 			if mod.height not in [1,2]:
 				raise RuntimeError(f"Module '{mod.name:s}' has invalid height {mod.height:d}")
 
-			if mod.width not in [1,2,3,4,8]:
+			if mod.width not in [1,2,3,4,6,8]:
 				raise RuntimeError(f"Module '{mod.name:s}' has invalid width {mod.width:d}")
 
 			if (mod.pos_x is not None) and (
