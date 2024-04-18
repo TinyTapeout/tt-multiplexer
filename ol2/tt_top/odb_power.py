cfb3c3737ae3b1074f74676535268d72847b9628
Uri Shaked <uri@urishaked.com>
2024-04-18 21:38:49 +0300

    ol2: Fix for Openlane 2.0.0 final
    We no longer need to manually add the "odbpy" directory to the python path, thanks to efabless/openlane2#449

--- ol2/tt_top/odb_power.py
+++ ol2/tt_top/odb_power.py
@@ -17,8 +17,6 @@ import tt_odb
 
 import click
 
-from openlane.common.misc import get_openlane_root
-sys.path.insert(0, os.path.join(get_openlane_root(), "scripts", "odbpy"))
 from reader import click_odb
 
 
