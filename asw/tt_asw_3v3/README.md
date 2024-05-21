87de06dd3e356c6537219f15027827732b735f14
Sylvain Munaut <tnt@246tNt.com>
2024-05-21 15:42:20 +0200

    asw/tt_asw_3v3: Prepare for new 3v3 rated analog switch
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

---
+++ asw/tt_asw_3v3/README.md
@@ -0,0 +1,5 @@
+![](../../workflows/lvs/badge.svg)
+
+# 3V3/5V tolerant analog switch for TT07+
+
+[schematic](https://xschem-viewer.com/?file=https://github.com/TinyTapeout/tt-multiplexer/blob/main/asw/tt_asw_3v3/xschem/tt_asw_3v3.sch)
---
+++ asw/tt_asw_3v3/src/tt_asw_3v3.v
@@ -0,0 +1,21 @@
+/*
+ * tt_asw_3v3.v
+ *
+ * Blackbox for the analog switches
+ *
+ * Copyright (c) 2024 Sylvain Munaut <tnt@246tNt.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+(* blackbox *)
+module tt_asw_3v3 (
+`ifdef USE_POWER_PINS
+	input  wire VGND,
+	input  wire VPWR,
+`endif
+	inout  wire mod,
+	inout  wire bus,
+	input  wire ctrl
+);
+
+endmodule
