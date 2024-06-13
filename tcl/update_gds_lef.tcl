82d5b1129fc24849a934c889c2ae1a3b6f3e979b
Sylvain Munaut <tnt@246tNt.com>
2024-06-13 17:20:50 +0200

    tcl: Flatten cell before exporting to GDS
    Avoids issue when there is some common sub cell name used
    by several blocks ...
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- tcl/update_gds_lef.tcl
+++ tcl/update_gds_lef.tcl
@@ -6,6 +6,21 @@
 set project [lindex $argv $argc-1]
 load $project
 
+# Flatten
+set bbox [property FIXED_BBOX]
+set psdm [property MASKHINTS_PSDM]
+set nsdm [property MASKHINTS_NSDM]
+
+select top cell
+flatten flat_tmp
+load flat_tmp
+cellname delete $project
+cellname rename flat_tmp $project
+
+property FIXED_BBOX $bbox
+property MASKHINTS_PSDM "[property MASKHINTS_PSDM] $psdm"
+property MASKHINTS_NSDM "[property MASKHINTS_NSDM] $nsdm"
+
 # GDS
 gds write ../gds/$project.gds
 
