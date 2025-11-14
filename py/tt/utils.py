cc135ec0c833262b2141e04c7afa3e732d347344
Sylvain Munaut <tnt@246tNt.com>
2025-11-14 20:06:26 +0100

    py: Add LayoutNode/LayoutDimension
    This is to prepare for support of scaling where OpenRoad internal
    units are not nanometers
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- py/tt/utils.py
+++ py/tt/utils.py
@@ -14,7 +14,7 @@ import yaml
 from collections import namedtuple
 
 
-__all__ = [ 'ConfigNode', 'Point', 'Rect' ]
+__all__ = [ 'ConfigNode', 'LayoutNode', 'Point', 'Rect', 'LayoutDimension' ]
 
 
 class ConfigNode:
@@ -71,6 +71,51 @@ class ConfigNode:
 		self.update_from_dict(yaml.load(stream, yaml.FullLoader))
 
 
+class LayoutNode:
+
+	def __init__(self, init=None):
+		self._dat = {}
+
+		if init is not None:
+			for k,v in init.items():
+				self[k] = v
+
+	def __getattr__(self, k):
+		try:
+			return self._dat[k]
+		except KeyError:
+			raise AttributeError()
+
+	def __setattr__(self, k, v):
+		if k[0] != '_':
+			# Process value
+			if isinstance(v, dict):
+				# Dict become LayoutNodes
+				v = self.__class__(v)
+
+			elif isinstance(v, int):
+				# Integer become LayoutDimension
+				v = LayoutDimension(v)
+
+			elif isinstance(v, self.__class__):
+				# Nothing to do
+				pass
+
+			else:
+				# Unsupported
+				raise ValueError('Unsupported type in LayoutNode')
+
+			self._dat[k] = v
+		else:
+			super().__setattr__(k,v)
+
+	def __getitem__(self, k):
+		return self._dat[k]
+
+	def __setitem__(self, k, v):
+		self.__setattr__(k, v)
+
+
 class Point(namedtuple('Point', 'x y')):
 
 	__slots__ = []
@@ -116,4 +161,65 @@ class Rect(namedtuple('Rect', 'x0 y0 x1 y1')):
 
 	def move(self, xofs, yofs):
 		return Rect(self.x0 + xofs, self.y0 + yofs, self.x1 + xofs, self.y1 + yofs)
+
+
+class LayoutDimension(int):
+
+	__iu_scale = 1000
+
+	@classmethod
+	def set_iu_scale(self, scale):
+		LayoutDimension.__iu_scale = scale
+
+	@classmethod
+	def from_iu(kls, iu):
+		return kls(1000 * iu // kls.__iu_scale)
+
+	def __new__(kls, value, *args, **kwargs):
+		return  super(kls, kls).__new__(kls, value)
+
+	def __add__(self, other):
+		return self.__class__(int(self) + int(other))
+
+	__radd__ = __add__
+
+	def __sub__(self, other):
+		return self.__class__(int(self) - int(other))
+
+	def __rsub__(self, other):
+		return self.__class__(int(other) - int(self))
+
+	def __mul__(self, other):
+		if isinstance(other, self.__class__):
+			raise ValueError("Can't multiply two LayoutDimension")
+		return self.__class__(int(self) * int(other))
+
+	def __rmul__(self, other):
+		if isinstance(other, self.__class__):
+			raise ValueError("Can't multiply two LayoutDimension")
+		return self.__class__(int(self) * int(other))
+
+	def __floordiv__(self, other):
+		if isinstance(other, self.__class__):
+			return int(self) // int(other)
+		else:
+			return self.__class__(int(self) // int(other))
+
+	def __str__(self):
+		return "%d" % int(self)
+
+	def __repr__(self):
+		return "LayoutDimension(%d nm)" % int(self)
+
+	@property
+	def nm(self):
+		return int(self)
+
+	@property
+	def um(self):
+		return self / 1000.0
+
+	@property
+	def iu(self):
+		return int(round(int(self) / 1000.0 * self.__iu_scale))
 
